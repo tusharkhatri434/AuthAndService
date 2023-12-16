2b991ec7df00d8628b1d28ddd2646bdd7ab0a09b
[tushar khatri] <tusharkhatri8193@gmail.com>
2023-12-16 14:20:27 +0530

    password hasing done

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,8 @@
 const dotEnv = require('dotenv');
-
+const bcrypt = require('bcrypt');
 dotEnv.config();
 
 module.exports = {
-    PORT:process.env.PORT
-}
+  PORT: process.env.PORT,
+  SALT: bcrypt.genSaltSync(9)
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const apiRoutes= require('./routes/index');
 const {PORT} = require('./config/serverConfig');
 
+//  const {User} = require('./models/index');
+//  const bcrypt = require('bcrypt');
+
 const setUpServer = async ()=>{
     const app = express();
     
@@ -12,7 +15,11 @@ const setUpServer = async ()=>{
     app.use('/api',apiRoutes);
     
     app.listen(PORT, async ()=>{
-        console.log(`server is listing on ${PORT}`);
+      console.log(`server is listing on ${PORT}`);
+        // const incomingpassword = '12345';
+        // const user = await User.findByPk(2);
+        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // console.log(response);
     });
 }
 
