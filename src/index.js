8fd415809662943a2a4c96aa231bea7cd8fe6f94
[tushar khatri] <tusharkhatri8193@gmail.com>
2023-12-18 20:44:53 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const apiRoutes= require('./routes/index');
 const {PORT} = require('./config/serverConfig');
 
-//  const {User} = require('./models/index');
+// const db = require('./models/index');
+
+//  const {User,Role} = require('./models/index');
 //  const bcrypt = require('bcrypt');
 
 const setUpServer = async ()=>{
@@ -20,6 +22,17 @@ const setUpServer = async ()=>{
         // const user = await User.findByPk(2);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
         // console.log(response);
+
+        // if(process.env.DB_sync){
+        //   db.sequelize.sync({ alter: true });
+        // }
+
+        // const u1 = await User.findByPk(1);
+        // const r1 = await Role.findByPk(2);
+        // // u1.addRole(r1);
+        // const response = await u1.hasRole(r1);
+        // console.log(response);
+
     });
 }
 
